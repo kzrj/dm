884d9f1f4bb83c4521c38b92e5f1d0a7b662837d
kaizerj <kzrster@gmail.com>
2020-07-22 14:55:29 +0800

    viber login

---
+++ dm/profiles/serializers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from rest_framework import serializers
+
+from profiles.models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'nickname', 'viber_name', 'viber_avatar']
