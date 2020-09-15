895d27ba0c6a436673225782b3934fb4bbde77b5
kaizerj <kzrster@gmail.com>
2020-09-15 15:18:45 +0800

    contact

--- dm/profiles/admin.py
+++ dm/profiles/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from profiles.models import Profile
+from profiles.models import Profile, Contact
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Profile._meta.fields]
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Contact._meta.fields]
