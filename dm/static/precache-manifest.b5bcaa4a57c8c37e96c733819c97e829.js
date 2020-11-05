ee062cf7d441bf71be3bc37ed3584251fa4f5ba9
kaizerj <kzrster@gmail.com>
2020-11-05 18:34:26 +0800

    add static

---
+++ dm/static/precache-manifest.b5bcaa4a57c8c37e96c733819c97e829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca6538c5c79ac424d3d55f5e23ed23b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d21771ca30671fc9cb",
+    "url": "/static/js/2.e955fac7.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.e955fac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557b60c93fca74deb918",
+    "url": "/static/js/main.051e8c59.chunk.js"
+  },
+  {
+    "revision": "a343276fe94f8fa38d78",
+    "url": "/static/js/runtime-main.639bd9e0.js"
+  }
+]);
