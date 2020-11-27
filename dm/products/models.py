fc54fef558402e29201f7fc2b877b19d24164187
kaizerj <kzrster@gmail.com>
2020-11-27 21:27:57 +0800

    static

--- dm/products/models.py
+++ dm/products/models.py
@@ -161,12 +161,12 @@ class ProductImageManager(CoreModelManager):
         product_image = self.create(product=product)
         product_pk = product.pk if product else 0
 
-        product_image.original.save(f'{product.pk}', image_file)
+        product_image.original.save(f'{product.pk}.jpg', image_file)
 
-        catalog_image_name = f'catalog_{product_image.original.name}'
+        catalog_image_name = f'catalog_{product_image.original.name}.jpg'
         product_image.catalog_image.save(catalog_image_name, catalog_image)
 
-        thumb_image_name = f'thumb_{product_image.original.name}'
+        thumb_image_name = f'thumb_{product_image.original.name}.jpg'
         product_image.thumb_image.save(thumb_image_name, thumb_image)
 
         return product_image
