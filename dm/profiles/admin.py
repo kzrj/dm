b7293794de28086c8b9e139db137279164271c4f
kzrj <kzrster@gmail.com>
2021-01-14 18:38:08 +0800

    socials

--- dm/profiles/admin.py
+++ dm/profiles/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from profiles.models import Profile
+from profiles.models import Profile, SocialLink
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Profile._meta.fields]
+
+
+@admin.register(SocialLink)
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in SocialLink._meta.fields]
