608dff299bab5678915cdf2fab1b383083acec82
kzrj <kzrster@gmail.com>
2021-01-27 00:36:27 +0800

    add suggestion

--- dm/feedbacks/admin.py
+++ dm/feedbacks/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from feedbacks.models import Feedback, Suggestion
+
+
+@admin.register(Suggestion)
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Suggestion._meta.fields]
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Feedback._meta.fields]
