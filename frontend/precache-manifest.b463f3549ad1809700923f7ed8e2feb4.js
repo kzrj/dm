6f36dcd099ca5cabea489c6b5fd652c26dd9dbdc
kzrj <kzrster@gmail.com>
2021-02-09 01:37:42 +0800

    front

---
+++ frontend/precache-manifest.b463f3549ad1809700923f7ed8e2feb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1540948dd698c02f53e523b5152e43fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f3e1f965f38e4a1fca",
+    "url": "/static/js/2.26312dad.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/static/js/2.26312dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39278be5e4edc5f4aeed",
+    "url": "/static/js/main.db638294.chunk.js"
+  },
+  {
+    "revision": "a343276fe94f8fa38d78",
+    "url": "/static/js/runtime-main.639bd9e0.js"
+  }
+]);
